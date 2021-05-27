bde4cb89d3857f7939baea84b7289c49950bdb19
maruice chen <maurice.chen@foxmail.com>
2021-05-28 00:01:21 +0800

    添加一个 getName() + 后缀 的方法

--- commons/src/main/java/com/github/dactiv/framework/commons/CacheProperties.java
+++ commons/src/main/java/com/github/dactiv/framework/commons/CacheProperties.java
@@ -74,7 +74,7 @@ public class CacheProperties implements Serializable {
      *
      * @return 缓存名称 + 后缀值，如果缓存名称没有 ":" 符号，会自动加上。
      */
-    public String getName(String suffix) {
-        return StringUtils.appendIfMissing(getName(),":") + suffix;
+    public String getName(Object suffix) {
+        return StringUtils.appendIfMissing(getName(),":") + suffix.toString();
     }
 }
